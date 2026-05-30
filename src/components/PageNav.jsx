89ce907b9413f93db62555b04cd62a516eb0cbec
nikoletta17 <ivanova.nicole.home@gmail.com>
2026-05-30 15:31:00 +0300

    Added initial pages and components structure, implemented base routing

---
+++ src/components/PageNav.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+import Logo from "./Logo";
+
+const navLinks = [
+  { path: "/pricing", label: "Pricing" },
+  { path: "/product", label: "Product" },
+  { path: "/login", label: "Login" },
+];
+
+export const PageNav = () => {
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+      <ul>
+        <NavItems />
+      </ul>
+    </nav>
+  );
+};
+
+function NavItems() {
+  return (
+    <>
+      {navLinks.map((link) => (
+        <li key={link.path}>
+          <NavLink to={link.path}>{link.label}</NavLink>
+        </li>
+      ))}
+    </>
+  );
+}
+
+export default PageNav;
